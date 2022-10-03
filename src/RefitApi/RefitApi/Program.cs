0f852f034c3726b386b0a5f7b88eeac408b61fad
Matheus Falcirolli <fmatheus8@gmail.com>
2022-10-03 12:40:44 -0300

    Consumo API utilizando Refit

---
+++ src/RefitApi/RefitApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using Refit;
+using RefitApi.Interface;
+using System;
+using System.Threading.Tasks;
+
+namespace RefitApi
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cepClient = RestService.For<IAPIService>("https://viacep.com.br");
+            Console.WriteLine("Digite o CEP: ");
+            var cep = Console.ReadLine();
+
+            var address = await cepClient.GetAddressInformation(cep);
+
+            Console.WriteLine($"Endereço: {address.Logradouro}");
+        }
+    }
+}
